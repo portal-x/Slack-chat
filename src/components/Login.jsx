d99c754ddd01310383304ae3a386ff3b8dab2eb5
Yurii Tkachuk <papirus.borzna@gmail.com>
2021-05-27 16:56:19 +0300

    add formik

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default () => {
   console.log('login');
-  return <div>login page</div>;
+  return (
+    <div className="card-body d-flex flex-column justify-content-around align-items-center p-5">login page</div>
+  );
 };
