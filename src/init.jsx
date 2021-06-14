cd28d92ba9b5e3be93fee12ca90fa72a187aae04
Yurii Tkachuk <papirus.borzna@gmail.com>
2021-06-14 17:06:45 +0300

    add redux

--- src/init.jsx
+++ src/init.jsx
@@ -7,9 +7,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './components/App.jsx';
+import store from './redux/store.js';
 
 export default () => {
-  ReactDOM.render(<App />, document.querySelector('#chat'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.querySelector('#chat'),
+  );
 };
